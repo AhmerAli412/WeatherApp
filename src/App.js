820f1ac0faf96ba00cf99b9deb1f77eee7a6af6c
AhmerAli412 <aliahmer289@gmail.com>
2022-04-09 19:42:03 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Navbar from './omponent/Navbar';
+import Sec from './omponent/Sec';
+
+import './index.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar/>
+        <Sec/>
+        
+    </div>
+  );
+}
+
+export default App;
